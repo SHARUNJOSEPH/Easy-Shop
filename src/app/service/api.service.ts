d9a9ae23f140025018e87a258dd3c24f075561d6
SHARUNJOSEPH <josephsharun9895@gmail.com>
2021-08-20 21:08:49 +0530

    "test"

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -10,7 +10,7 @@ import { catchError, retry } from 'rxjs/operators';
 export class ApiService {
   constructor(private http: HttpClient) {}
   getProduct() {
-    return this.http.get<any>('https://fakestoreapi.com/products').pipe(
+    return this.http.get<any>('http://fakestoreapi.com/products').pipe(
       map((res: any) => {
         return res;
       })
